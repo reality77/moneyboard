a5a3a4654fe06defd80d61e8186daeffb64b4547
reality77 <davmail08-misc@yahoo.fr>
2017-10-28 01:11:41 +0200

    [WEB] Integrating axios & first import page

--- src/web/ClientApp/components/account_import/account_import.ts
+++ src/web/ClientApp/components/account_import/account_import.ts
@@ -1,17 +1,48 @@
 import Vue from 'vue';
-//import VueResource from 'vue-resource';
+import Axios from 'axios';
+
 import { Component } from 'vue-property-decorator';
 
+interface ImportedAccount {
+    transactions: ImportedTransaction[];
+}
+
+interface ImportedTransaction {
+    transactionDate: Date;
+    captionOrPayee: string;
+    category: string;
+    transferTarget: string;
+    amount: number;
+    memo: string;
+    number: string;
+    detectionSucceded: boolean;
+    detectedUserDate: Date;
+    detectedMode: string;
+    detectedComment: string;
+    detectedCaption: string;
+    detectedPayeeId: number;
+    detectedCategoryId: number;
+    importTransactionHash: string;
+}
+
 @Component
-export default class AccountsComponent extends Vue {
-    accounts: Account[] = [];
+export default class AccountTransactionsImportComponent extends Vue {
+    accounts: ImportedAccount[] = [];
 
     onFileChange(e: any) {
 
-        /*this.$http({ url: '/someUrl', method: 'GET' }).then(function (response:any) {
-            // success callback
-        }, function (response:any) {
-            // error callback
-        });*/
+        //var files = e.target.files || e.dataTransfer.files;
+        var formData = new FormData();
+
+        formData.append("file", e.target.files[0]);
+
+        Axios.post('http://localhost:49871/import/prepare', formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
+        })
+        .then(response => response.data as Promise<ImportedAccount[]>)
+        .then(data => { this.accounts = data; })
+        .catch(error => console.log(error));
     };
 }
