3cdb7d74e9aa87c25abacd8759c6d98ce7cd2e2c
reality77 <davmail08-misc@yahoo.fr>
2017-10-30 10:54:21 +0100

    [API] [WEB] Setting debug URLs

--- src/webapp/web/Program.cs
+++ src/webapp/web/Program.cs
@@ -20,6 +20,7 @@ namespace web
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseUrls("http://0.0.0.0:56779")
                 .Build();
     }
 }
